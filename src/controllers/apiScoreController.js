76cb3cea14146337ae964c0307528c35c5768555
Larissa R. Gabriel <rademakerlarissa@gmail.com>
2026-05-19 17:07:34 -0300

    atulizando o salvamento de score da api

--- src/controllers/apiScoreController.js
+++ src/controllers/apiScoreController.js
@@ -7,14 +7,9 @@ export async function save(req, res) {
     const {id_user, newScore} = req.body;
 
     const user = await getUserById(id_user);
+    
+    await saveScore(id_user, newScore);
 
-    if (newScore > user.score) {
-        
-        await saveScore(id_user, newScore);
-
-        return res.status(200).json({ message: "Pontuação salva com sucesso" });
-    } else {
-        return res.status(200).json({ message: "Pontuação não atualizada, nova pontuação é menor que a atual" });
-    }
+    return res.status(200).json({ message: "Pontuação salva com sucesso" });
 
 }
