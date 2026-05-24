18cd5f785fb4512a29f33c857dc53c3feba525ba
Matheus <mathk004@gmail.com>
2026-05-24 14:44:24 -0300

    removendo trechos de código que não deveriam estar no local

--- src/view/jogar.js
+++ src/view/jogar.js
@@ -1,11 +1,6 @@
-export function telaJogar(navegarPara) {
+export function telaJogar() {
     document.getElementById('app').innerHTML = `
         <h1>Esse é jogar</h1>
         <button id="btn-voltar">Voltar</button>
     `;
-    const btnVoltar = document.getElementById('btn-voltar');
-    
-    btnVoltar.addEventListener('click', () => {
-        navegarPara('menu');
-    });
 }
--- src/view/ranking.js
+++ src/view/ranking.js
@@ -1,11 +1,6 @@
-export function telaRanking(navegarPara) {
+export function telaRanking() {
     document.getElementById('app').innerHTML = `
         <h1>Esse é o Ranking</h1>
         <button id="btn-voltar">Voltar</button>
     `;
-    const btnVoltar = document.getElementById('btn-voltar');
-    
-    btnVoltar.addEventListener('click', () => {
-        navegarPara('menu');
-    });
 }
